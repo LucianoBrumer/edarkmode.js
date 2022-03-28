bb0f98267f012c7cc1b647b39c2fd76869212027
LucianoBrumer <lucianobrumer5@gmail.com>
2022-03-27 21:42:00 -0300

    [200~more readable~

--- src/edarkmode.js
+++ src/edarkmode.js
@@ -8,6 +8,12 @@ const setTheme = t => {
 darkmodeToggleButton.addEventListener("click", e => localStorage.getItem("theme") == "light" ? setTheme("dark") : setTheme("light"));
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", e => setTheme(e.matches ? "dark" : "light"));
 
-localStorage.getItem("theme") == null ? ((window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) ? setTheme("dark") : setTheme("light")) : localStorage.getItem("theme") == "dark" ? setTheme("dark") : setTheme("light");
+localStorage.getItem("theme") == null
+	? ((window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches)
+		? setTheme("dark")
+		: setTheme("light"))
+	: localStorage.getItem("theme") == "dark"
+		? setTheme("dark")
+		: setTheme("light");
 
 module.exports = setTheme;
